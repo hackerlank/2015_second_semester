85e927b2862347edadbe479fcec981d99c27ef73
Woong Bin Kim <woongnistelrooy@hanmail.net>
2015-09-15 18:46:51 +0900

    윈프_3주차

---
+++ 2. window_programming/ch03/ex01/MainFrm.cpp
@@ -0,0 +1,145 @@
+
+// MainFrm.cpp : CMainFrame 클래스의 구현
+//
+
+#include "stdafx.h"
+#include "ex01.h"
+
+#include "MainFrm.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+// CMainFrame
+
+IMPLEMENT_DYNAMIC(CMainFrame, CFrameWnd)
+
+BEGIN_MESSAGE_MAP(CMainFrame, CFrameWnd)
+	ON_WM_CREATE()
+	ON_WM_SETFOCUS()
+	ON_WM_CLOSE()
+END_MESSAGE_MAP()
+
+static UINT indicators[] =
+{
+	ID_SEPARATOR,           // 상태 줄 표시기
+	ID_INDICATOR_CAPS,
+	ID_INDICATOR_NUM,
+	ID_INDICATOR_SCRL,
+};
+
+// CMainFrame 생성/소멸
+
+CMainFrame::CMainFrame()
+{
+	// TODO: 여기에 멤버 초기화 코드를 추가합니다.
+}
+
+CMainFrame::~CMainFrame()
+{
+}
+
+int CMainFrame::OnCreate(LPCREATESTRUCT lpCreateStruct)
+{
+	if (CFrameWnd::OnCreate(lpCreateStruct) == -1)
+		return -1;
+
+	// 프레임의 클라이언트 영역을 차지하는 뷰를 만듭니다.
+	if (!m_wndView.Create(NULL, NULL, AFX_WS_DEFAULT_VIEW, CRect(0, 0, 0, 0), this, AFX_IDW_PANE_FIRST, NULL))
+	{
+		TRACE0("뷰 창을 만들지 못했습니다.\n");
+		return -1;
+	}
+
+	if (!m_wndStatusBar.Create(this))
+	{
+		TRACE0("상태 표시줄을 만들지 못했습니다.\n");
+		return -1;      // 만들지 못했습니다.
+	}
+	m_wndStatusBar.SetIndicators(indicators, sizeof(indicators)/sizeof(UINT));
+
+	
+	//key랑 value를 변수에 저장ㄱㄱ
+	
+	/*
+	cs.x = l;
+	cs.y = t;
+	cs.cx = w;
+	cs.cy = h;
+	*/
+	return 0;
+}
+
+BOOL CMainFrame::PreCreateWindow(CREATESTRUCT& cs)
+{
+	if( !CFrameWnd::PreCreateWindow(cs) )
+		return FALSE;
+	// TODO: CREATESTRUCT cs를 수정하여 여기에서
+	//  Window 클래스 또는 스타일을 수정합니다.
+
+	cs.dwExStyle &= ~WS_EX_CLIENTEDGE;
+	cs.lpszClass = AfxRegisterWndClass(0);
+
+	int l = AfxGetApp()->GetProfileInt("jmlee", "left", 0); //key(jmlee)랑 value(left)를 불르고, 혹시 없다면 0을 넣어라
+	int t = AfxGetApp()->GetProfileInt("jmlee", "top", 0);
+	int w = AfxGetApp()->GetProfileInt("jmlee", "width", -1);
+	int h = AfxGetApp()->GetProfileInt("jmlee", "height", -1);
+	
+	
+	if(w != -1) {
+		this->MoveWindow(l, t, w, h, 1);
+	}
+
+	return TRUE;
+}
+
+// CMainFrame 진단
+
+#ifdef _DEBUG
+void CMainFrame::AssertValid() const
+{
+	CFrameWnd::AssertValid();
+}
+
+void CMainFrame::Dump(CDumpContext& dc) const
+{
+	CFrameWnd::Dump(dc);
+}
+#endif //_DEBUG
+
+
+// CMainFrame 메시지 처리기
+
+void CMainFrame::OnSetFocus(CWnd* /*pOldWnd*/)
+{
+	// 뷰 창으로 포커스를 이동합니다.
+	m_wndView.SetFocus();
+}
+
+BOOL CMainFrame::OnCmdMsg(UINT nID, int nCode, void* pExtra, AFX_CMDHANDLERINFO* pHandlerInfo)
+{
+	// 뷰에서 첫째 크랙이 해당 명령에 나타나도록 합니다.
+	if (m_wndView.OnCmdMsg(nID, nCode, pExtra, pHandlerInfo))
+		return TRUE;
+
+	// 그렇지 않으면 기본 처리합니다.
+	return CFrameWnd::OnCmdMsg(nID, nCode, pExtra, pHandlerInfo);
+}
+
+
+void CMainFrame::OnClose()
+{
+	// TODO: 여기에 메시지 처리기 코드를 추가 및/또는 기본값을 호출합니다.
+	
+	CRect r;//사각형에 대한 클래스
+	this->GetWindowRect(&r);
+	
+	//registry에 정보 전달하기
+	AfxGetApp()->WriteProfileInt("jmlee", "left", r.left); //key(jmlee)랑 value(left) 저장
+	AfxGetApp()->WriteProfileInt("jmlee", "top", r.top);
+	AfxGetApp()->WriteProfileInt("jmlee", "width", r.Width());
+	AfxGetApp()->WriteProfileInt("jmlee", "height", r.Height());
+
+	CFrameWnd::OnClose();
+}
