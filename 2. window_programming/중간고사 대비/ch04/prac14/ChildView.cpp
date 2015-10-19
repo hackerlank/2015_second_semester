e4a26010c91d3fa09f13b6732b9900c87be65a35
Woong Bin Kim <woongnistelrooy@hanmail.net>
2015-10-19 15:58:45 +0900

    10_19 윈프 중간고사 대비

--- 2. window_programming/중간고사 대비/ch04/prac14/ChildView.cpp
+++ 2. window_programming/중간고사 대비/ch04/prac14/ChildView.cpp
@@ -25,6 +25,7 @@ CChildView::~CChildView()
 BEGIN_MESSAGE_MAP(CChildView, CWnd)
 	ON_WM_PAINT()
 	ON_WM_LBUTTONDOWN()
+	ON_WM_LBUTTONDBLCLK()
 END_MESSAGE_MAP()
 
 
@@ -47,7 +48,8 @@ BOOL CChildView::PreCreateWindow(CREATESTRUCT& cs)
 void CChildView::OnPaint() 
 {
 	CPaintDC dc(this); // 그리기를 위한 디바이스 컨텍스트입니다.
-	//비트맵 리소스를 로드한 후 크기 정보를 얻는다. 
+	/*
+	//비트맵 리소스를 로드한 후 크기 정보를 얻는다.
 	CBitmap bitmap;
 	bitmap.LoadBitmapA(IDB_BITMAP1);
 	BITMAP bmpinfo;
@@ -61,10 +63,11 @@ void CChildView::OnPaint()
 	//비트맵을 화면에 출력한다. 
 	dc.BitBlt(10, 10, bmpinfo.bmWidth, bmpinfo.bmHeight, &dcmem, 0, 0, SRCCOPY);
 	/*dc.StretchBlt(10, 100, bmpinfo.bmWidth*4, bmpinfo.bmHeight*2, &dcmem, 0, 0, bmpinfo.bmWidth, 
-		bmpinfo.bmHeight, SRCCOPY);*/
+		bmpinfo.bmHeight, SRCCOPY);
 	dcmem.Rectangle(5, 5, 15, 15);
 
 	dc.BitBlt(350, 50, bmpinfo.bmWidth, bmpinfo.bmHeight, &dcmem, 0, 0, SRCCOPY);
+	*/
 	
 }
 
@@ -87,12 +90,13 @@ void CChildView::OnLButtonDown(UINT nFlags, CPoint point)
 	CRect r;
 	this->GetClientRect(&r);
 	
-
+	dc.BitBlt(100, 100, bm.bmWidth, bm.bmHeight, &mdc, 0, 0, SRCCOPY);
+	Invalidate();
 	if(nFlags == MK_LBUTTON) {
-		dc.BitBlt(100, 100, bm.bmWidth, bm.bmHeight, &mdc, 0, 0, SRCCOPY);
-	} else {
 		dc.StretchBlt(10, 10, r.Width(), r.Height(), &mdc, 0, 0, bm.bmWidth, bm.bmHeight, SRCCOPY);
-	}
-	this->Invalidate();
+		
+	} 
+	
 	CWnd::OnLButtonDown(nFlags, point);
 }
+
--- 2. window_programming/중간고사 대비/ch04/prac14/ChildView.h
+++ 2. window_programming/중간고사 대비/ch04/prac14/ChildView.h
@@ -34,5 +34,6 @@ protected:
 	DECLARE_MESSAGE_MAP()
 public:
 	afx_msg void OnLButtonDown(UINT nFlags, CPoint point);
+	
 };
 
