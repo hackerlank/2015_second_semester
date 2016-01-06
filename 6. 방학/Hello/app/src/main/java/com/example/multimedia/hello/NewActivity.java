7b756b5961b3f9fce982513830f1781d2cec8207
Woong Bin Kim <woongnistelrooy@hanmail.net>
2016-01-06 13:19:15 +0900

    12
    12

---
+++ 6. 방학/Hello/app/src/main/java/com/example/multimedia/hello/NewActivity.java
@@ -0,0 +1,25 @@
+package com.example.multimedia.hello;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+public class NewActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new);
+    }
+
+    public void onBackButtonClicked(View v) {
+        Toast.makeText(getApplicationContext(), "돌아가기 버튼이 눌렸어요", Toast.LENGTH_LONG).show();
+        finish();
+    }
+
+}
